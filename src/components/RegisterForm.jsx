fe1994c26269ddc9f4268b75dac0fa1fb15dd4a7
Felipe Ramirez <feliperamirezloaiza10@gmail.com>
2025-08-31 17:54:14 -0500

    Feat: Creating Registro.jsx components

---
+++ src/components/RegisterForm.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import Input from "./Input.jsx"
+import Button from "./Button.jsx"
+
+function RegisterForm() {
+  return (
+    <form id="register">
+      <Input type="email" name="correo" id="email" placeholder="Correo electrónico" />
+      <br />
+      <Input type="password" name="contraseña" id="clave" placeholder="Contraseña" />
+      <br />
+      <h2>-Mínimo 12 caracteres</h2>
+      <h2>-Debe contener una mayúscula</h2>
+      <h2>-Debe contener un número</h2>
+      <Input type="password" name="confirm" id="confirmClave" placeholder="Confirmar contraseña" />
+      <br /><br />
+      <Button type="submit" text="Registrarse" />
+
+      <div className="register">
+        <p>
+          ¿Ya tienes una cuenta?&nbsp;&nbsp;&nbsp;
+          <Link to="/inicio">Iniciar sesión</Link>
+        </p>
+      </div>
+    </form>
+  );
+}
+
+export default RegisterForm
--- src/pages/Registro.jsx
+++ src/pages/Registro.jsx
@@ -1,27 +1,15 @@
 import React from "react"
-import { Link } from "react-router-dom"
 import "../assets/css/StyleRegistro.css"
+import RegisterForm from "../components/RegisterForm"
 
 function Registro() {
   return (
-    <div className="container">
-        <div className="form-background">
-            <h1>REGISTRARSE</h1>
-            <form id="register">
-                <input type="email" name="Email" id="email" placeholder="Correo electrónico" />
-                <br/>
-                <input type="password" name="contrasena" id="clave" placeholder="Contraseña" />
-                <h2>Mínimo 12 caracteres</h2>
-                <h2>Debe contener una mayúscula</h2>
-                <h2>Debe contener un Número</h2>
-                <input type="password" name="confirm" id="confirmClave" placeholder="Confirmar contraseña" />
-                <br/><br/>
-                <input type="submit" value="Registrarse" />
-                <div className="login">
-                    <p>¿Ya tienes una cuenta?&nbsp;&nbsp;&nbsp;<Link to="/inicio">Iniciar sesión</Link></p><br/>
-                </div>
-            </form>
-        </div>
+    <div className="registro-page">
+      <div className="container">
+        <h1>REGISTRARSE</h1>
+          <br /><br />
+          <RegisterForm />
+      </div>
     </div>
   )
 }
